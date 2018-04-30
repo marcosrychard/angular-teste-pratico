fab463e86137c055defc84667c1d7b8d14b97453
marcos.andrade <marcos.andrade@neppoadm.com.br>
2018-04-30 13:59:41 -0300

    make it better

--- src/server/config/database.js
+++ src/server/config/database.js
@@ -1,7 +1,8 @@
 module.exports = (app) => {
     const mongoose = require('mongoose');
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost:27017/cedro');
+    const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost:27017/angular-teste-papratico'
+    mongoose.connect(url);
     mongoose.connection.on('error', () => {
         console.log('Could not connect to the database. Exiting now...');
         process.exit();
